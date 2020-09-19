577969ca4cf98a549da3ba7a5ee7ccc9fb10da6d
Victor-web-dot <68923681+Victor-web-dot@users.noreply.github.com>
2020-09-19 22:54:34 +0300

    Дурка
    Добавил функцию при которой бот не будет слетать при капче а будет просить ее у вас. Также вырезал блокировку паролем

--- RaidBotSource.py
+++ RaidBotSource.py
@@ -2,12 +2,9 @@ import random
 import time
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-botpass=input('Введите пароль: ')
-if(botpass=='b523aqAPzE2JEgSUnQQ9MrwMDGbmpGwV'):
-    print('Бот успешно разблокирован. Теперь пригласите страничку подключенную к нему в беседу и пропишите там команду: Начать')
-    vk_session = vk_api.VkApi(token='')
-    longpoll = VkLongPoll(vk_session)
-    vk = vk_session.get_api()
+vk_session = vk_api.VkApi(token='',captcha_handler=captcha_handler)
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
             if event.text == 'Начать' or event.text == '/start':
